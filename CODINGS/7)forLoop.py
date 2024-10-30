f431f19fbe7662bc9182aa93ea3fbaf8e89ae8f1
sandyddeveloper <sandyddeveloper@gmail.com>
2024-10-30 14:45:21 +0530

    *

--- CODINGS/7)forLoop.py
+++ CODINGS/7)forLoop.py
@@ -1,2 +1,111 @@
-for i in range(1, 10):
-    print(i)
+for i in range(1,6):# this is row
+    if i in [1, 5]:
+        for j in range(1,4):
+            if j in [1,2,3]:
+                print("*", end=" ")
+            else:
+                print(" ", end=" ")
+        print()
+        
+    elif i== 2:
+        for j in range(1,4):
+            if j in [2]:
+                print("*", end=" ")
+            else:
+                print(" ", end=" ")
+        print()
+    elif i ==3:
+        for j in range(1,4):
+            if j in [2]:
+                print("*", end=" ")
+            else:
+                print(" ", end=" ")
+        print()
+    else :
+        for j in range(1,4):
+            if j in [2]:
+                print("*", end=" ")
+            else:
+                print(" ", end=" ")
+        print()
+    
+    
+for i in range (1,6):
+    if i in [1, 5]:
+        for j in range(1, 6):
+            if j in [1,5]:
+                print("*", end=" ")
+            else:
+                print(" ", end=" ")
+        print()
+    elif i == 2 :
+        for j in range(1, 6):
+            if j in [2,4]:
+                print("*", end=" ")
+            else:
+                print(" ", end=" ")
+        print()
+    elif i == 4 :
+        for j in range(1, 6):
+            if j in [2,4]:
+                print("*", end=" ")
+            else:
+                print(" ", end=" ")
+        print()
+    else:
+        for j in range(1,6):
+            if j in [3]:
+                print("*", end=" ")
+            else:
+                print(" ", end= " ")
+        print()
+        
+for i in range(1, 8):
+    for j in range(1, 6):
+        if (i == 1 or i == 4 or i == 7) and (1 <= j <= 5):
+            print("*", end=" ")
+        elif (i == 2 or i == 3) and j == 1:
+            print("*", end=" ")
+        elif (i == 5 or i == 6) and j == 5:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+
+for i in range (1,6):
+    for j in range(1,6):
+        if (i == 1 or i == 3 or i == 5):
+            print("*", end=" ")
+        elif (i == 2 or i == 4 ) and j == 1:
+            print("*" , end=" ")
+        else:
+            print(" ", end= " ")
+    print()
+
+
+for i in range(1,6):
+    for j in range(1,6):
+        if( i == 1 or i == 5):
+            print("*", end=" ")
+        elif (i==2) and j ==4:
+            print("*", end=" ")
+        elif (i==3) and j == 3:
+            print("*", end=" ")
+        elif (i==4) and j == 2:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+        
+        
+for i in range(1,6):
+    for j in range(1,6):
+        if (i == 1 or i == 3 or i == 5) and j in (1,2,3,5):
+            print("*", end=" ")
+        elif (i == 2 or i == 4) and j in (3,5):
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+            
+            
--- CODINGS/ANSWERS/Ex1.py
+++ CODINGS/ANSWERS/Ex1.py
@@ -1,39 +1,40 @@
-# One
-name = str(input("Enter your name:"))
-rollno = int(input("Enter your rollno:"))
+# # One
+# name = str(input("Enter your name:"))
+# rollno = int(input("Enter your rollno:"))
 
-print("My name is:",name)
-print("My rollno:",rollno)
+# print("My name is:",name)
+# print("My rollno:",rollno)
 
-#Two
-name1 = input("Enter the name:")
-rollno1 = int(input("Enter your rollno:"))
-address = input("Enter your address:")
+# #Two
+# name1 = input("Enter the name:")
+# rollno1 = int(input("Enter your rollno:"))
+# address = input("Enter your address:")
 
-print("My name is:", name1)
-print("My rollno is:", rollno1)
-print("My address is:", address)
+# print("My name is:", name1)
+# print("My rollno is:", rollno1)
+# print("My address is:", address)
 
-#Three
-a = int(input('First Number'))
-b = int(input('Second Number'))
-c = int(input('Three Number'))
+# #Three
+# a = int(input('First Number'))
+# b = int(input('Second Number'))
+# c = int(input('Three Number'))
 
-mul = a*b*c
-add = a+b+c
-div = mul/add
+# mul = a*b*c
+# add = a+b+c
+# div = mul/add
 
-print("Multiply Answer:", mul)
-print("Added Answer:", add)
-print("Divide Answer:", div)
+# print("Multiply Answer:", mul)
+# print("Added Answer:", add)
+# print("Divide Answer:", div)
 
-#Four
-names = input("Enter your Name:")
-score = int(input("Enter your score:"))
-department = input("enter your dept:")
+# #Four
+# names = input("Enter your Name:")
+# score = int(input("Enter your score:"))
+# department = input("enter your dept:")
 
-get_score = score/10
+# get_sore = score/10
 
-print("Student Name:", names)
-print("My Score is:",get_score,"/10")
-print("My Department is:", department)
+# print("Student Name:", names)
+# print("My Score is:",get_score,"/10")
+# print("My Department is:", department)
+
